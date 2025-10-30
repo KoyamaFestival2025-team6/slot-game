6ae0a71889cb6b378ed923d6d024111c811aaba7
Sunshine <a2004724kazukiz16@gmail.com>
2025-10-31 02:22:45 +0900

    リールの色がついた最終的なモデルを取り込んだ &  タイトル -> ゲーム画面までのカメラの動きの制御を実装

---
+++ Assets/Scripts/Slot/CameraManager.cs
@@ -0,0 +1,23 @@
+﻿using Unity.Cinemachine; // Cinemachineの名前空間を忘れずに
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    // インスペクターで設定する
+    [SerializeField] CinemachineCamera titleCamera;
+    [SerializeField] CinemachineCamera gameCamera;
+
+    // カメラAをアクティブにする（Priorityを高くする）
+    public void ChangeTitleCamera()
+    {
+        titleCamera.Priority = 20; // 高い優先度
+        gameCamera.Priority = 10; // 低い優先度
+    }
+
+    // カメラBをアクティブにする（Priorityを高くする）
+    public void ChangeGameCamera()
+    {
+        titleCamera.Priority = 10; // 低い優先度
+        gameCamera.Priority = 20; // 高い優先度
+    }
+}
